6148dba73e12e0cca673f0934974c15daafaacc6
Gilberto Silva <gilberto.silva@sigga.com>
2021-01-24 15:55:28 -0300

    Implementado o calculo da idade e tratado a visualização do resultado.

--- Idade de Cachorro/Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/Idade de Cachorro/ViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var dogAgeTextField: UITextField!
+    @IBOutlet weak var resultTextLabel: UILabel!
+    var resultText:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultText = resultTextLabel.text!
+        resultTextLabel.isHidden = true
     }
-
+    
     @IBAction func calculateAge(_ sender: Any) {
-        resultText.text = "Texto alterado"
+        let result = Int(dogAgeTextField.text!)! * 7
+        resultTextLabel.isHidden = false
+        resultTextLabel.text = resultText + String(result)
     }
     
 }
